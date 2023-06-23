e4197faecc9d3f67b775df8d07276ac1f53ba753
cheol <qjel60@naver.com>
2023-06-23 12:14:42 +0900

    feat : governance, timelock

---
+++ contracts/governance.sol
@@ -0,0 +1,105 @@
+//SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Governance {
+    address private owner;
+    address private govToken;
+    uint256 private proposalAmount;
+
+    struct Receipt {
+        bool vote;
+        bool agree;
+    }
+
+    struct Proposal {
+        address proposer;
+        uint startBlock;
+        uint endBlock;
+        bytes callData;
+        bool canceled;
+        bool executed;
+        uint256 amountVote;
+        mapping(address => Receipt) hasVotes;
+    }
+
+    address[] public participants;
+
+    mapping(uint => Proposal) public proposes;
+    mapping(uint => address[]) public votes;
+
+    constructor(address _owner){
+        owner = _owner;
+    }
+
+    function propose (address _proposer, bytes memory _callData) public {
+        require(IERC20(govToken).balanceOf(_proposer) > 0);
+
+        uint startBlock = block.number; 
+        uint endBlock = block.number + 17280; 
+        Proposal storage newProposal = proposes[proposalAmount + 1];
+        newProposal.proposer = _proposer;
+        newProposal.startBlock = startBlock;
+        newProposal.endBlock = endBlock;
+        newProposal.callData = _callData;        
+        newProposal.canceled = false;
+        newProposal.executed = false;
+        newProposal.amountVote = 0;
+        newProposal.hasVotes[_proposer] = Receipt(true, true);
+        proposalAmount += 1;
+    }
+
+    function voting (address _participant, uint _proposal, bool _agree) public {
+        require(IERC20(govToken).balanceOf(_participant) > 0, "Governance : ");
+        require(proposes[_proposal].hasVotes[_participant].vote == false);
+        require(proposes[_proposal].endBlock > block.number);
+        proposes[_proposal].hasVotes[_participant] = Receipt(true, _agree);
+        votes[_proposal].push(_participant);
+        proposes[_proposal].amountVote += uint256(IERC20(govToken).balanceOf(_participant) * 10**18)/IERC20(govToken).totalSupply();
+    }
+
+    function excute (uint _proposal) public returns(bool){
+        require(msg.sender == owner);
+        require(proposes[_proposal].endBlock < block.number); //3일 > 17,280
+        if(proposes[_proposal].amountVote > 0.51 * 10**18) {
+            proposes[_proposal].executed = true;
+            //타임락 구현 된 곳 으로 보내기
+            // return true;
+        } else {
+            proposes[_proposal].canceled = true;
+            // return false;
+        }
+    }
+
+    function makeCallData () public returns(bytes memory) {
+        return abi.encode(msg.data);
+    }
+
+    function changeLevel (address token) public {
+        
+    }
+
+    function changeOwner (address _newOwner) private {
+        owner = _newOwner;
+    }
+
+    function getTokenAddress (address _token) public {
+        require(owner == msg.sender);
+        govToken = _token;
+    }
+
+    function getProposal(uint _idx) public view returns (address, uint, uint, bytes memory, bool, bool, uint) {
+    Proposal storage proposal = proposes[_idx];
+    return (
+        proposal.proposer,
+        proposal.startBlock,
+        proposal.endBlock,
+        proposal.callData,
+        proposal.canceled,
+        proposal.executed,
+        proposal.amountVote
+    );
+}
+
+}
---
+++ contracts/timelcok.sol
@@ -0,0 +1,236 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting with custom message on overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, errorMessage);
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on underflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot underflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction underflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on underflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot underflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, errorMessage);
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers.
+     * Reverts on division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers.
+     * Reverts with custom message on division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+contract Timelock {
+    using SafeMath for uint;
+
+    address admin;
+
+    struct StableState {
+        bool status;
+        bool canceled;
+        uint256 blockTime;
+    }
+
+    mapping(bytes32 => StableState) public proposals;
+
+    uint public constant GRACE_PERIOD = 14 days;
+    uint public constant MINIMUM_DELAY = 2 days;
+    uint public constant MAXIMUM_DELAY = 30 days;
+
+    uint delay;
+
+    constructor(uint _delay, address _admin){
+        require(delay > MINIMUM_DELAY);
+        require(delay < MAXIMUM_DELAY);
+
+        admin = _admin;
+        delay = _delay;
+    }
+
+    function setDelay (uint _delay)public {
+        require(delay > MINIMUM_DELAY);
+        require(delay < MAXIMUM_DELAY);
+
+        delay = _delay;
+    }
+
+    function queueTransaction (bytes memory callData, uint256 _blockTime) public {
+        bytes32 hashdata = keccak256(callData);
+        if(proposals[hashdata].blockTime == 0){
+            proposals[hashdata] = StableState(false, false, _blockTime);
+        }
+    }
+
+    function cancelTransaction (bytes memory callData) public returns(bool){
+        require(msg.sender == admin);
+        bytes32 hashdata = keccak256(callData);
+        proposals[hashdata].canceled = true;
+        return true;
+    }
+
+    function excuteTransaction(bytes memory callData, uint256 etc) public returns(bool){
+        require(msg.sender == admin);
+        bytes32 hashdata = keccak256(callData);
+        require(etc > proposals[hashdata].blockTime.add(delay));
+        proposals[hashdata].status = true;
+        return true;
+    }
+
+
+    function getTransaction (bytes memory callData) public view returns(StableState memory) {
+        bytes32 hashdata = keccak256(callData);
+        return proposals[hashdata];
+    }
+
+}
+
